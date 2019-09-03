a59e008a3394b3e1826b0a5fa95aa181ff2ea2d3
Diwash Mulmi <addlelife@gmail.com>
2019-09-03 16:24:53 +0200

    Commit excelParser with Maven template.

---
+++ com.excelParser/src/main/java/com/excelParser/ReadExcel.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.excelParser;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Diwash_M Read excel file
+ *
+ */
+public class ReadExcel {
+	public static final String COVERSHEETS_FILE_PATH = ""
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException, InvalidFormatException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
