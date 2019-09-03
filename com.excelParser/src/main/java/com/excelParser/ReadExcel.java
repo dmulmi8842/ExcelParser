e0573cdce069d505726a8ba2ca5c88592447515b
Diwash Mulmi <addlelife@gmail.com>
2019-09-03 17:55:16 +0200

    Commit modified1 ReadExcel main class

--- com.excelParser/src/main/java/com/excelParser/ReadExcel.java
+++ com.excelParser/src/main/java/com/excelParser/ReadExcel.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.excelParser;
+package main.webapp.com.excelParser;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
@@ -56,6 +56,34 @@ public class ReadExcel {
 		//Getting sheet with name "Progressive Info"
 		Sheet sheet = workbook.getSheet("Progressive Info");
 		
+		// Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+        
+     // Obtain a rowIterator and columnIterator and iterate over them
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        while (rowIterator.hasNext()) {
+        	Row row  =  rowIterator.next();
+        	
+        	// Now let's iterate over the columns of the current row
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+        
+		/*
+		 * // 2. Use a for-each loop to iterate over the rows and columns System.out.
+		 * println("\n\nIterating over Rows and Columns using for-each loop\n"); for
+		 * (Row row: sheet) { for(Cell cell: row) { String cellValue =
+		 * dataFormatter.formatCellValue(cell); System.out.print(cellValue + "\t"); }
+		 * System.out.println(); }
+		 */
+        
+        workbook.close();
 		
 	}
 
